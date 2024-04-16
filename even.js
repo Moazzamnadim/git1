52cf8a64746ef5085a9a29262dbc902d154a7b92
Moazzam Nadimullah <moazzamnadimullah@Moazzams-MacBook-Air.local>
2024-04-16 19:52:52 +0530

    added palindrome file

--- even.js
+++ even.js
@@ -1,12 +1,22 @@
-let obj={
-    length:5,
-     width:20,
-     area: function(){
-       console.log(this.length*this.width)
-     },
-     perimeter: function() {
-     console.log(2*(this.length+this.width));
-     }
-   }
-   obj.perimeter()
-   obj.area()
+// let num=4
+// if(num%2==0){
+//   console.log("Even number")
+// }
+// else{
+//   console.log("Odd number")
+// }
+
+
+
+// let obj={
+//     length:5,
+//      width:20,
+//      area: function(){
+//        console.log(this.length*this.width)
+//      },
+//      perimeter: function() {
+//      console.log(2*(this.length+this.width));
+//      }
+//    }
+//    obj.perimeter()
+//    obj.area()
---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let name="naman"
+let bag="";
+for(let i= i<=name.length-1; i>=0; i--){
+    bag=bag+name[i]
+}
+if (name==bag){
+  
+    console.log("palindrome")
+}
+else{
+    console.log("Not palindrome")
+}
